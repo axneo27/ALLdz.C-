511abc9a677704acc1ce7959fd9f23e9a9150039
axneo27 <163739774+axneo27@users.noreply.github.com>
2024-09-14 17:00:27 +0300

    Add files via upload

---
+++ dz1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void song() {
+	cout << "Coldplay, Yellow:\n\n Look at the stars\n Look how they shine for you\n\n And everything you do\n\n Yeah, they were all yellow\n I came along\n I wrote a song for you\n And all the things you do\n\n And it was called Yellow" << endl;
+}
+
+void everyhunter() {
+	cout << "Every" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\t\tWants" << "\n";
+	cout << "\t\t\tto Know" << "\n";
+	cout << "\t\t\t\t Where" << "\n";
+	cout << "\t\t\t\t\tdoes Pheasant" << "\n";
+	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+}
+
+void ogolosh() {
+	cout << "\t\t\t\t\t\tANNOUNCEMENT" << "\n\n";
+	cout << "\t\t\t\t\t  SEEKING FOR A PROGRAMMER" << endl;
+	cout << "\t\t\t\t\t  REQUIREMENTS: " << "\n\n";
+
+	cout << "\t\t\t\t\t  # must be willing to work 36\n\t\t\t\t\t hours a day, 9 days a week;" << "\n\n";
+	cout << "\t\t\t\t\t  # teleportation experience\n\t\t\t\t\t preferred but not required;" << endl;
+	cout << "\t\t\t\t\t  # ability to read minds;" << "\n\n";
+	cout << "\t\t\t\t\t  # bonus points if you’ve\n\t\t\t\t\t ever coded while skydiving \n\t\t\t\t\t (and it worked);" << "\n\n";
+	cout << "\t\t\t\t\t  # bonus points if you think\n\t\t\t\t\t in binary." << "\n\n";
+
+	cout << "\t\t\t\t\tOur Contacts: +380 99 999 99 99" << endl;
+	cout << "\t\t\t\t\tAdress: One Apple Park Way, Cupertino\n\t\t\t\t\t, CA 95014" << endl;
+
+}
+
+int main() {
+	song();
+	everyhunter();
+	ogolosh();
+
+	return 0;
+}
+
